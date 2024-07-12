7184c7def05ae352e05c8601a6c68a38758ea097
Satyajeet Behera <jagajajpur3@gmail.com>
2024-07-12 14:47:28 +0530

    adding components for login page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/auth/login-form";
+const login = () => {
+  return (
+    <div>
+      <LoginForm></LoginForm>
+    </div>
+  );
+};
+export default login;
---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
+import { Header } from "./header";
+import { Social } from "./social";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-full md:w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel}></Header>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+                <CardFooter>
+                    <Social />
+                </CardFooter>
+            )}
+    </Card>
+  );
+};
