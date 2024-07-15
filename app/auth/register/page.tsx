a3245c696cbf0ae9922025938d23f0b9cc47602f
Satyajeet Behera <jagajajpur3@gmail.com>
2024-07-15 13:55:34 +0530

    Create page.tsx

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "@/components/auth/register-form";
+const ResisterPage = () => {
+  return (
+    <div>
+      <RegisterForm></RegisterForm>
+    </div>
+  );
+};
+export default ResisterPage;
