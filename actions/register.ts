7e369259c84fcf59c799ef0599d8147f031a7bff
Satyajeet Behera <jagajajpur3@gmail.com>
2024-07-17 10:31:56 +0530

    Update register.ts

--- actions/register.ts
+++ actions/register.ts
@@ -1,15 +1,47 @@
 "use server"
 import * as z from "zod"
+import * as bcrypt from "bcrypt"
+
 import { RegisterSchema } from "@/schemas"
+import { db } from "@/lib/db"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
-    if(!validatedFields.success){
+    if (!validatedFields.success) {
         return {
-            error : "Inavalid fields"
+            error: "Inavalid fields"
         }
     }
+
+
+    const { email, password, name } = validatedFields.data
+
+
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const existingUser = await db.user.findUnique({
+        where:{
+            email,
+        }
+    })
+
+    if(existingUser){
+        return {
+            error:"Email already Taken!"
+        }
+    }
+
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password:hashedPassword,
+        }
+    })
+    
     return {
-        sucess : "Email sucessuflly send to Backend"
+        sucess: "You have make a ID in our Database"
     }
 }
