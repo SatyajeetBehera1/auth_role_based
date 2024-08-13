0626d6da41786ccf768762fc8f68f9732b562b81
Satyajeet Behera <jagajajpur3@gmail.com>
2024-08-14 01:27:37 +0530

    Update register.ts

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,7 @@ import * as bcrypt from "bcrypt"
 
 import { RegisterSchema } from "@/schemas"
 import { db } from "@/lib/db"
+import { getUserByEmail } from "@/data/user"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
@@ -21,12 +22,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword = await bcrypt.hash(password, salt)
 
-    const existingUser = await db.user.findUnique({
-        where:{
-            email,
-        }
-    })
-
+    const existingUser = await getUserByEmail(email)
     if(existingUser){
         return {
             error:"Email already Taken!"
