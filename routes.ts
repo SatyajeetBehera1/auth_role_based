d7d323ee45049c5b13badb7f66f9085af5b43fd6
Satyajeet Behera <jagajajpur3@gmail.com>
2024-08-16 21:15:37 +0530

    add a middleware

--- routes.ts
+++ routes.ts
@@ -4,7 +4,10 @@ export const publicRoutes = ["/", "/auth/new-verification"];
 export const authRoutes = [
     "/auth/login",
     "/auth/register",
-  ];
+];
   
 
-  export const apiAuthPrefix = "/api/auth";
+export const apiAuthPrefix = "/api/auth";
+
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
