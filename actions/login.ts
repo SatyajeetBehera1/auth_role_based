2f57b9dc65d6668556585cbad353a7ee42a262a1
Satyajeet Behera <jagajajpur3@gmail.com>
2024-08-28 22:40:14 +0530

    Update login.ts

--- actions/login.ts
+++ actions/login.ts
@@ -1,15 +1,38 @@
 "use server"
 import * as z from "zod"
 import { LoginSchema } from "@/schemas"
-
+import { signIn } from "@/auth"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
+import { AuthError } from "next-auth"
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     const validatedFields = LoginSchema.safeParse(values)
-    if(!validatedFields.success){
+    if (!validatedFields.success) {
         return {
-            error : "Inavalid fields"
+            error: "Inavalid fields"
         }
     }
-    return {
-        sucess : "Email sucessuflly send to Backend"
+
+    const { email, password } = validatedFields.data
+
+    try {
+        await signIn("credentials", {
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT,
+        })
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case "CredentialsSignin":
+                    return {
+                        error: "Inavalid Credentials!"
+                    }
+                default:
+                    return {
+                        error: "Something went wrong"
+                    }
+            }
+        }
+        throw error
     }
 }
